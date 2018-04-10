bcd8293d03204dfb5ec83c280af77e8cbefae9c0
mkhalil7625 <mkhalil7625@gmail.com>
2018-04-10 18:15:46 -0500

    atropix app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var apodService=require('../services/apod');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('fetchpicture');
 });
+// fetch a pic from NASA's astronomy picture of the day service
+router.get('/fetchpicture', function (req, res, next) {
+    apodService(function (err, apod_data) {
+        if (err) {
+            res.render('apod_error', {message: err.message, title: 'Error'});
+
+        } else {
+            res.render('index', {apod: apod_data, title: 'APOD for ${apod_data}'});
+        }
+    },req.query.picturetype);
+    });
+   // if (req.query.picturetype==='random'){
+     // res.send('todo:get random picture');
+    // }else{
+    //  // res.send('todo: get today\'s picture')
+    // }
 
 module.exports = router;
